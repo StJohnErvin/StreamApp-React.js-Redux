ceab69d939b5675710c146c1144f58d9a8cb01d3
john <johnjamero007@gmail.com>
2021-11-25 17:01:10 +0800

    fixed action types

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
+const INTIAL_STATE = {
+  isSignedIn: null
+};
+
+export default (state = INTIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false };
+    default:
+      return state;
+  }
+};
