9aceddef292dcf45464a7c894d8d6c06e1cb768e
john <johnjamero007@gmail.com>
2021-11-26 11:11:11 +0800

    forms with validation and error message

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
+import StreamCreate from './streams/StreamCreate'
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
