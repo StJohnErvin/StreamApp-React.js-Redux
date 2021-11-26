31645b43766b185933aa3076a00ad564801b2bc2
john <johnjamero007@gmail.com>
2021-11-26 13:05:04 +0800

    create stream with rest conventions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,10 @@
+import streams from '../apis/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
 
-export const signIn = () => {
+export const signIn = userId => {
   return {
-    type: SIGN_IN
+    type: SIGN_IN,
+    payload: userId
   };
 };
 
@@ -10,4 +12,8 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+export const createStream = formValues => async dispatch => {
+  streams.post('/streams', formValues);
 };
